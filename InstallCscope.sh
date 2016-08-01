ca96ec214384e336c71806c41c94994fc66811d3
Kyungmin Lee <shumin@naver.com>
2016-08-01 10:21:23 +0900

    Modify files for cscope

---
+++ InstallCscope.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mkdir ~/local/bin
+cp mkcscope.sh ~/local/bin/
+
+####################################################
+# Add the following path at ~/.bashrc
+#
+# export PATH=/home/shumin/pintos/src/utils:$PATH
+####################################################
--- cscope.sh
+++
@@ -1,1 +0,0 @@
-find . \(-name '*.c' -o -name '*.cpp' -o -name '*.cc' -o -name '*.h' -o -name '*.s' -o -name '*.S' \) -print > cscope.files
